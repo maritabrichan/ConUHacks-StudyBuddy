017a1330e33693849898f4a0311bfe90d141445a
Marita Brichan <maritabarikhan@hotmail.com>
2023-01-21 21:16:02 -0500

    Initial Commit

---
+++ StudyBuddy Watch App/StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy Watch App
+//
+//  Created by Marita Brichan on 2023-01-21.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
