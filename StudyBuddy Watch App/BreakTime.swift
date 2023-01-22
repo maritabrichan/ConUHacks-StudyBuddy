b86b9a90eff1bc2e1967b5d86879deb39a418289
Marita Brichan <maritabarikhan@hotmail.com>
2023-01-21 23:51:22 -0500

    added break time page

--- StudyBuddy Watch App/BreakTime.swift
+++ StudyBuddy Watch App/BreakTime.swift
@@ -5,4 +5,40 @@
 //  Created by Marita Brichan on 2023-01-21.
 //
 
-import Foundation
+import SwiftUI
+
+struct BreakTime: View {
+    
+    var body: some View {
+        ScrollView{
+      
+            VStack {
+                Text("It's Break Time! 😉").font(.system(size: 23.0))
+                Text("\nYou have worked very hard in the past 25 minutes and deserve some rest. 🍻\n").font(.system(size: 14.0))
+                
+                // Adding the button
+                Button (action: {
+                    
+                    
+                    // Code to execute when button is tapped goes here
+                    print("Button tapped!")
+                } ){
+                    Text("✨OK✨")
+                }  .buttonStyle(.bordered)
+                    .tint(.green)
+
+            }
+
+                
+            
+        }
+
+        .padding()
+    }
+}
+
+struct BreakTime_Previews: PreviewProvider {
+    static var previews: some View {
+        BreakTime()
+    }
+}
